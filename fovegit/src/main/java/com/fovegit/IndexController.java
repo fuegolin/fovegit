0479c2a07492e683a8c8ac978661cd723b923885
fuegolin_pc <569416592@qq.com>
2016-01-08 10:32:34 +0800

    fovegit second commit； from fuego(mucfc)

--- fovegit/src/main/java/com/fovegit/IndexController.java
+++ fovegit/src/main/java/com/fovegit/IndexController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by zhengxuelin on 2016/1/5.
  */
 @Controller
-@RequestMapping(value="/index")
+@RequestMapping(value="/jovegit")
 public class IndexController {
     private static Logger logger = Logger.getLogger(IndexController.class);
 
-    @RequestMapping(value="/home")
+    @RequestMapping(value="/index")
     public String homepage() {
         logger.info("进入主页");
         //return "WEB-INF/views/home.jsp";
-        return "index";
+        return "/home";
     }
+
+    
+
 }
--- fovegit/src/main/webapp/WEB-INF/view/home.jsp
+++ fovegit/src/main/webapp/WEB-INF/view/home.jsp
@@ -1,10 +1,14 @@
-<html>
+<html lang="zh-cn">
 <head>
+
     <link rel="stylesheet" type="text/css" href="../../resources/js/semantic/semantic.min.css">
     <script src="../../resources/js/semantic/semantic.min.js"></script>
+    <script src="../../resources/js/jquery-1.11.1.min.js"></script>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 </head>
 <body>
 <h1 class="ui header">The first page use semantic ui</h1>
+<div>引导页</div>
 <div>引导页</div>
 
 </body>
